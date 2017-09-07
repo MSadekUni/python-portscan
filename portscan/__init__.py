31355bf467a0428907773e98e9557beb8061dd20
Daniel Thurau <daniel.n.thurau@gmail.com>
2017-09-07 12:21:49 -0700

    BSD license added and specified in __init__.py

--- portscan/__init__.py
+++ portscan/__init__.py
@@ -4,5 +4,5 @@ __author__ = 'Daniel Thurau'
 __credits__ = ['Daniel Thurau']
 __maintainer__ = 'Daniel Thurau'
 __email__ = 'daniel.n.thurau@gmail.com'
-__license__ = ''
+__license__ = 'BSD'
 __version__ = '2.1.0'
