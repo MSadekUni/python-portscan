64c20bace5d432bfc7e4d8a120e34b1d297cbfeb
Daniel Thurau <daniel.n.thurau@gmail.com>
2017-09-07 14:17:50 -0700

    Moving logging startup info to main()

--- portscan/Log.py
+++ portscan/Log.py
@@ -1,5 +1,6 @@
 import logging
 import datetime
+import os
 import time
 
 __all__ = [
@@ -9,3 +10,14 @@ __all__ = [
 def send_log(message):
   """Logging method used throughout Kali_Port_Scanning"""
   logging.info(datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S') + " " + message)
+
+# stuff to run always here such as class/def
+def main():
+  FULL_PATH = os.path.dirname(os.path.realpath(__file__)) + "/"
+  LOG_FILE = FULL_PATH + ".log"
+  logging.basicConfig(filename=LOG_FILE, level=logging.INFO)
+
+
+if __name__ == "__main__":
+  # stuff only to run when not called via 'import' here
+  main()
