1a04b7feedc1ca598ca743086076863b336ae8b6
Daniel Thurau <daniel.n.thurau@gmail.com>
2017-09-08 13:21:23 -0700

    Generated gh-pages for commit a8123d3 Author: Daniel Thurau <daniel.n.thurau@gmail.com>
        all in one subprovess

--- portscan/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-__author__ = 'Daniel Thurau'
-__credits__ = ['Daniel Thurau']
-__maintainer__ = 'Daniel Thurau'
-__email__ = 'daniel.n.thurau@gmail.com'
-__license__ = 'BSD'
-__version__ = '1.0.0'
