25abed6e2261fb99b190adcc27b0cce8151ce58c
Daniel Thurau <daniel.n.thurau@gmail.com>
2017-09-11 10:37:52 -0700

    moved log lovation to /var/log/python-portscan

--- portscan/Log.py
+++ portscan/Log.py
@@ -7,17 +7,19 @@ __all__ = [
     'send_log'
 ]
 
+LOG_FILE = "/var/log/python-portscan/log"
+os.system("mkdir -p /var/log/python-portscan && touch /var/log/python-portscan/log")
+logging.basicConfig(filename=LOG_FILE, level=logging.INFO)
+
+
 def send_log(message):
   """Logging method used throughout python-portscan"""
   logging.info(datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S') + " " + message)
 
 # stuff to run always here such as class/def
-def main():
-  FULL_PATH = os.path.dirname(os.path.realpath(__file__)) + "/"
-  LOG_FILE = FULL_PATH + ".log"
-  logging.basicConfig(filename=LOG_FILE, level=logging.INFO)
+#def main():
 
 
-if __name__ == "__main__":
+#if __name__ == "__main__":
   # stuff only to run when not called via 'import' here
-  main()
+#  main()
